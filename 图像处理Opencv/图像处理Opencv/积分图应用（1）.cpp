8a9ee76d2466ea4eb3a1ade197da5c590aeba516
xiaozitaitiao <1109452106@qq.com>
2019-08-05 16:10:47 +0800

    1

--- 图像处理Opencv/图像处理Opencv/积分图应用（1）.cpp
+++ 图像处理Opencv/图像处理Opencv/积分图应用（1）.cpp
@@ -3,44 +3,44 @@
 
 using namespace std;
 using namespace cv;
-mat stdlocal(const mat &i,int radius)
+Mat stdLocal(const Mat &I,int radius)
 {
-	int rows = i.rows;
-	int cols = i.cols;
-	mat src;
-	copymakeborder(i, src, radius, radius, radius, radius, border_replicate);//扩展边界
-	src.convertto(src, cv_32fc1);
-	mat stdlocal = mat::zeros(rows,cols, cv_32fc1);
+	int rows = I.rows;
+	int cols = I.cols;
+	Mat src;
+	copyMakeBorder(I, src, radius, radius, radius, radius, BORDER_REPLICATE);//扩展边界
+	src.convertTo(src, CV_32FC1);
+	Mat stdLocal = Mat::zeros(rows,cols, CV_32FC1);
 	for (int i=radius;i<rows+radius;i++)
 	{
 		for (int j=radius;j<cols+radius;j++)
 		{
-			mat imgrio = mat(src, rect(j - radius, i - radius, 2 * radius+1, 2 * radius+1));
+			Mat imgRIO = Mat(src, Rect(j - radius, i - radius, 2 * radius+1, 2 * radius+1));
 
-			float meanvalue = mean(imgrio).val[0];
-			mat diff(imgrio.rows, imgrio.cols, cv_32fc1);
-			diff = imgrio - meanvalue;
+			float meanValue = mean(imgRIO).val[0];
+			Mat diff(imgRIO.rows, imgRIO.cols, CV_32FC1);
+			diff = imgRIO - meanValue;
 			diff = diff.mul(diff);
-			stdlocal.at<float>(i-radius,j-radius) = (mean(diff).val[0]);
+			stdLocal.at<float>(i-radius,j-radius) = (mean(diff).val[0]);
 
 
 		}
 	}
-	return stdlocal;
+	return stdLocal;
 }
 
 //***********利用积分图加速*************************//
-mat faststdlocal(const mat &i, int radius)
+Mat fastStdLocal(const Mat &I, int radius)
 {
-	int n = pow(2 * radius + 1, 2);//邻域内像素点个数
-	int rows = i.rows;
-	int cols = i.cols;
-	mat src;
-	copymakeborder(i, src, radius, radius, radius, radius, border_replicate);//扩展边界
-	src.convertto(src, cv_32fc1);
-	mat stdlocal = mat::zeros(rows, cols, cv_64fc1);
-	mat srcintegral,src2integral;
-	integral(src, srcintegral, src2integral, cv_64f);//求积分图
+	int N = pow(2 * radius + 1, 2);//邻域内像素点个数
+	int rows = I.rows;
+	int cols = I.cols;
+	Mat src;
+	copyMakeBorder(I, src, radius, radius, radius, radius, BORDER_REPLICATE);//扩展边界
+	src.convertTo(src, CV_32FC1);
+	Mat stdLocal = Mat::zeros(rows, cols, CV_64FC1);
+	Mat srcIntegral,src2Integral;
+	integral(src, srcIntegral, src2Integral, CV_64F);//求积分图
 
 
 	for (int i = radius; i < rows + radius; i++)
@@ -48,47 +48,47 @@ mat faststdlocal(const mat &i, int radius)
 		for (int j = radius; j < cols + radius; j++)
 		{
 			
-			point p11(i - radius ,j - radius);
-			point p12(i - radius, j + radius+1);
-			point p21(i + radius+1, j - radius);
-			point p22(i + radius+1, j + radius+1);
-			double sumrio_src2 = src2integral.at<double>(p22.x,p22.y) + src2integral.at<double>(p11.x, p11.y) - src2integral.at<double>(p12.x,p12.y) - src2integral.at<double>(p21.x,p21.y) ;
-			double sumrio_src = srcintegral.at<double>(p22.x, p22.y) + srcintegral.at<double>(p11.x, p11.y) - srcintegral.at<double>(p12.x, p12.y) - srcintegral.at<double>(p21.x, p21.y);
+			Point p11(i - radius ,j - radius);
+			Point p12(i - radius, j + radius+1);
+			Point p21(i + radius+1, j - radius);
+			Point p22(i + radius+1, j + radius+1);
+			double sumRIO_src2 = src2Integral.at<double>(p22.x,p22.y) + src2Integral.at<double>(p11.x, p11.y) - src2Integral.at<double>(p12.x,p12.y) - src2Integral.at<double>(p21.x,p21.y) ;
+			double sumRIO_src = srcIntegral.at<double>(p22.x, p22.y) + srcIntegral.at<double>(p11.x, p11.y) - srcIntegral.at<double>(p12.x, p12.y) - srcIntegral.at<double>(p21.x, p21.y);
 
-			stdlocal.at<double>(i - radius, j - radius) = (sumrio_src2 - (sumrio_src * sumrio_src / n)) / n;
+			stdLocal.at<double>(i - radius, j - radius) = (sumRIO_src2 - (sumRIO_src * sumRIO_src / N)) / N;
 
 
 		}
 	}
-	//normalize(stdlocal, stdlocal, 0, 255, norm_minmax);//归一化
-	//convertscaleabs(stdlocal, stdlocal);
-	return stdlocal;
+	//normalize(stdLocal, stdLocal, 0, 255, NORM_MINMAX);//归一化
+	//convertScaleAbs(stdLocal, stdLocal);
+	return stdLocal;
 }
 
 int main()
 {
-	mat src= imread("c:/users/11094/desktop/美颜1.jpg",0);
+	Mat src= imread("C:/Users/11094/Desktop/美颜1.jpg",0);
 	imshow("原图",src);
 
-	double time0 = static_cast<double>(gettickcount());
-	mat resut1 = stdlocal(src, 1);
-	time0 = ((double)gettickcount() - time0 )/ gettickfrequency();
-	cout << "stdlocal() time:" << time0<<endl;
+	double time0 = static_cast<double>(getTickCount());
+	Mat resut1 = stdLocal(src, 1);
+	time0 = ((double)getTickCount() - time0 )/ getTickFrequency();
+	cout << "stdLocal() time:" << time0<<endl;
 
-	double time1 = static_cast<double>(gettickcount());
-	mat resut2 = faststdlocal(src, 1);
-	time1 = ((double)gettickcount() - time1) / gettickfrequency();
-	cout << "faststdlocal() time:" << time1<<endl;
+	double time1 = static_cast<double>(getTickCount());
+	Mat resut2 = fastStdLocal(src, 1);
+	time1 = ((double)getTickCount() - time1) / getTickFrequency();
+	cout << "fastStdLocal() time:" << time1<<endl;
 
 	imshow("标准差", resut1);
 	imshow("积分图加速-标准差", resut2);
-	normalize(resut1, resut1, 0, 255, norm_minmax);
-	normalize(resut2, resut2, 0, 255, norm_minmax);
-	convertscaleabs(resut1, resut1);
-	convertscaleabs(resut2, resut2);
+	normalize(resut1, resut1, 0, 255, NORM_MINMAX);
+	normalize(resut2, resut2, 0, 255, NORM_MINMAX);
+	convertScaleAbs(resut1, resut1);
+	convertScaleAbs(resut2, resut2);
 	imshow("归一化-标准差", resut1);
 	imshow("积分图加速-归一化标准差", resut2);
-	waitkey(0);
+	waitKey(0);
 
 	return 0;
 }
