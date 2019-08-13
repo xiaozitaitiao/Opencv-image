cdc44261025c9ed44a9375ffe5693787d107c77f
xiaozitaitiao <1109452106@qq.com>
2019-08-13 14:44:29 +0800

    Revert "11"
    This reverts commit e9d15f9735a274af8d599469c64b85249674d3f7.

--- 图像处理Opencv/图像处理Opencv/积分图应用（2）-NIM.cpp
+++ 图像处理Opencv/图像处理Opencv/积分图应用（2）-NIM.cpp
@@ -1,62 +1,28 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+
 using namespace std;
-using namespace cv;
-void fastNLM(const Mat &input_img, Mat& output_img, int ds, int Ds, int p)
+using namespace  cv;
+/*
+I:input image
+ds:radius of neighborhood window
+Ds;radius of search window
+a;smoothing parameter of gaussian function
+*/
+Mat &&NLMfilter(Mat &I, int ds, int Ds, int a)
 {
-	/*
-	ds:template window size
-	Ds:search window size
-	p:Gasuuian parameter
-	*/
-	int rows = input_img.rows;
-	int cols = input_img.cols;
-	int pixls = pow(2 * ds + 1, 2);//模板窗口内所有像素量
-	Mat src, paddedV;
-	copyMakeBorder(input_img, src, Ds + ds, Ds + ds, Ds + ds, Ds + ds, BORDER_REPLICATE);
-	copyMakeBorder(input_img, paddedV, Ds, Ds, Ds, Ds, BORDER_REPLICATE);
-	paddedV.convertTo(paddedV, CV_64FC1);
-	src.convertTo(src, CV_64FC1);
-	Mat average = Mat::zeros(rows, cols, CV_64FC1);
-	Mat Weight = Mat::zeros(rows, cols, CV_64FC1);
-	for (int t1 = -Ds; t1 <= Ds; t1++)
+	if (I.channels!=1)
 	{
-		for (int t2 = -Ds; t2 <= Ds; t2++)
-		{
-			Mat diff = src(Range(Ds + t1, rows + Ds + 2 * ds + t1), Range(Ds + t2, cols + Ds + 2 * ds + t2)) - src(Range(Ds, rows + Ds + 2 * ds), Range(Ds, cols + Ds + 2 * ds));
-			pow(diff, 2, diff);
-			Mat diffIntegral;
-			integral(diff, diffIntegral, CV_64FC1);
-			Mat V = paddedV(Range(Ds + t1, rows + Ds + t1), Range(Ds + t2, cols + Ds + t2));
-			for (int i = 0; i < rows; i++)
-			{
-				for (int j = 0; j < cols; j++)
-				{
-					Point p1(i + 2 * ds + 1, j + 2 * ds + 1);
-					Point p2(i, j);
-					double dist = diffIntegral.at<double>(p1.x, p1.y) + diffIntegral.at<double>(p2.x, p2.y) - diffIntegral.at<double>(p1.x, p2.y) - diffIntegral.at<double>(p2.x, p1.y);
-					dist = exp(-dist / (pixls*p));
-					Weight.at<double>(i, j) += dist;
-					average.at<double>(i, j) += dist * V.at<double>(i, j);
-				}
-			}
-
-
-
-		}
+		cout << "I`channel must be 1" << endl;
+		exit(0);
 	}
-	output_img = average.mul(1 / Weight);
-	convertScaleAbs(output_img, output_img);
-}
+	Mat src;
+	I.convertTo(src, CV_32FC1);
+	src = copyMakeBorder(src,src,);
+	int rows = I.rows;
+	int cols = I.cols;
 
 
-int main()
-{
-	Mat src = imread("C:/Users/11094/Desktop/美颜2.jpg", 0);
-	Mat result1;
-	Mat result2;
-	fastNlMeansDenoising(src, result1);
-	fastNLM(src, result2, 7, 21, 15);
-	return 0;
+
 
 }
